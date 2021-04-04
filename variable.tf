645951169bf6c4cef0fe26ec3cf552913badbb05
root <root@ip-172-31-95-142.ec2.internal>
2021-04-04 01:18:30 +0000

    key

--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
 variable "webserver_region" {default = "us-east-1"}
 variable "webserver_instanceType" {default = "t2.micro"}
 variable "webserver_ami" {default = "ami-042e8287309f5df03"}
-variable "webserver_access_key" {default = "AKIARJAFN37A22FFH5KG"}
-variable "webserver_secret_key" {default = "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"}
+variable "webserver_access_key" {default = "AKIARJAFN37AYBM4KCSH"}
+variable "webserver_secret_key" {default = "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"}
 variable "webserver_vpc_block" {default = "10.0.0.0/16"}
 variable "webserver_subnet_block" {default = "10.0.1.0/24"}
